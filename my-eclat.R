91568e392f344985517cddf62fc8682d5105ac3a
Ayush Sahu <ayushsahu99@gmail.com>
2019-03-05 00:52:35 +0530

    Add files via upload

---
+++ my-eclat.R
@@ -0,0 +1,15 @@
+# Eclat
+
+# Data Preprocessing
+# install.packages('arules')
+library(arules)
+dataset = read.csv('Market_Basket_Optimisation.csv', header = FALSE)
+dataset = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
+summary(dataset)
+itemFrequencyPlot(dataset, topN = 10)
+
+# Training Eclat in the dataset
+rules = eclat(data = dataset, parameter = list(support = 0.003, minlen = 2))
+
+# Visualising the Results
+inspect(sort(rules, by = 'support')[1:10])
